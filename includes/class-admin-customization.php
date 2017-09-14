9a432d34898394c6f946d57b934fa2b1281d52c3
Anurag Singh <contact@anuragsingh.me>
2017-09-14 13:32:49 +0530

    add more tabs

--- includes/class-admin-customization.php
+++ includes/class-admin-customization.php
@@ -179,7 +179,7 @@ class Admin_Customization {
 	public function admin_enqueue_styles ( $hook = '' ) {
 		wp_register_style( $this->_token . '-admin', esc_url( $this->assets_url ) . 'css/admin.css', array(), $this->_version );
 		wp_register_style( 'font-awesome-admin', esc_url( $this->assets_url ) . 'css/font-awesome.min.css', array(), $this->_version );
-		
+
 		wp_enqueue_style( $this->_token . '-admin' );
 		wp_enqueue_style( 'font-awesome-admin' );
 	} // End admin_enqueue_styles ()
@@ -275,4 +275,75 @@ class Admin_Customization {
 		update_option( $this->_token . '_version', $this->_version );
 	} // End _log_version_number ()
 
+
+	/* Remove Comments */
+	public function get_all_selected_posts () {
+		$post_types = get_option('as_hide_comments_for_post_types');
+		return $post_types;
+	}
+	public function as_hide_comments() {
+		add_action('admin_init', 'df_disable_comments_post_types_support');
+		$post_types = $this->get_all_selected_posts();
+		if(in_array('all', $post_types)){
+			add_filter('comments_open', 'df_disable_comments_status', 20, 2);
+			add_filter('pings_open', 'df_disable_comments_status', 20, 2);
+			add_filter('comments_array', 'df_disable_comments_hide_existing_comments', 10, 2);
+			add_action('admin_menu', 'df_disable_comments_admin_menu');
+			add_action('admin_init', 'df_disable_comments_admin_menu_redirect');
+			add_action('admin_init', 'df_disable_comments_dashboard');
+			add_action('init', 'df_disable_comments_admin_bar');
+			add_action( 'wp_before_admin_bar_render', 'my_admin_bar_render' );
+		}
+
+	}
+	/* Remove Comments */
+
 }
+
+
+
+// Disable support for comments and trackbacks in post types
+	function df_disable_comments_post_types_support() {
+		//$post_types = get_post_types();
+		$post_types = get_option('admin_customization_hide_comments_for_post_types');
+		foreach ($post_types as $post_type) {
+			if(post_type_supports($post_type, 'comments')) {
+				remove_post_type_support($post_type, 'comments');
+				remove_post_type_support($post_type, 'trackbacks');
+			}
+		}
+	}
+	// Close comments on the front-end
+	function df_disable_comments_status() {
+		return false;
+	}
+	// Hide existing comments
+	function df_disable_comments_hide_existing_comments($comments) {
+		$comments = array();
+		return $comments;
+	}
+	// Remove comments page in menu
+	function df_disable_comments_admin_menu() {
+		remove_menu_page('edit-comments.php');
+	}
+	// Redirect any user trying to access comments page
+	function df_disable_comments_admin_menu_redirect() {
+		global $pagenow;
+		if ($pagenow === 'edit-comments.php') {
+			wp_redirect(admin_url()); exit;
+		}
+	}
+	// Remove comments metabox from dashboard
+	function df_disable_comments_dashboard() {
+		remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+	}
+	// Remove comments links from admin bar
+	function df_disable_comments_admin_bar() {
+			if (is_admin_bar_showing()) {
+					remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+			}
+	}
+	function my_admin_bar_render() {
+		global $wp_admin_bar;
+		$wp_admin_bar->remove_menu('comments');
+	}
