6e38824ba778cfeb15c4d21810bc4eb3a4bc1848
Anurag Singh <contact@anuragsingh.me>
2017-09-15 20:25:06 +0530

    disable comment

--- includes/class-admin-customization-settings.php
+++ includes/class-admin-customization-settings.php
@@ -899,6 +899,23 @@ class Admin_Customization_Settings {
 		}
 	}
 	/* Add Favicon */
+	
+	/* Disable Notification */
+remove_action('load-update-core.php','wp_update_plugins');
+add_filter('pre_site_transient_update_plugins','__return_null');
+
+add_action('admin_menu','wphidenag');
+function wphidenag() {
+remove_action( 'admin_notices', 'update_nag', 3 );
+}
+
+function remove_core_updates(){
+global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates');
+add_filter('pre_site_transient_update_plugins','remove_core_updates');
+add_filter('pre_site_transient_update_themes','remove_core_updates');
+/* Disable Notification */
 
 
 
